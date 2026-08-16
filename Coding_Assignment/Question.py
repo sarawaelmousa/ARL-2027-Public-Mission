5a5db35557d91839ca40ca721fe0558ea1468a04
Sara Mousa <sarawaelmousa@gmail.com>
2026-08-16 22:25:27 +0300

    Update Question.py

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -1,14 +1,13 @@
-import test_following_distance.py 
 import math
 
-
-def monitor_following_distance(distances: list[float], speeds: list[float]) 
+def monitor_following_distance(distances: list[float], speeds: list[float]) :
         avrg_speed = (speeds[0] + speeds[1] + speeds[2] + speeds[3] + speeds[4])/5
         safe_dist = avrg_speed*0.5
 
-        count = 0
-        tailgate_dist = set()
+        count = 0                # counter to get the seconds
+        tailgate_dist = set()        #set of tailgate distances so i can arrange it and get the 1st value as min distance and the length of the set as incedents
         
+
         for n in distances :
             if n < safe_dist:
                 count += 1
@@ -23,4 +22,6 @@ def monitor_following_distance(distances: list[float], speeds: list[float])
     
         result = (tailgating_sec , min_dist , tailgate_inc)
     return results 
+
+print (monitor_following_distance(distances: list[float], speeds: list[float]))
 
